9754a84b5d6dbab2d697b76cf8e8227d217ddbde
Sachin Arora <sachinarora0986@gmail.com>
2024-09-06 21:17:26 +0530

    lINK Added

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const API_URL = "https://jsonplaceholder.typicode.com/users";
 
-export const GITHUB_URL = "https://github.com/sachin0986/All-Ticks";
+export const GITHUB_URL = "https://github.com/sachin0986/synergy-labs-assignment";
 
 export const LINKEDIN_URL = "https://www.linkedin.com/in/sachin0986";
